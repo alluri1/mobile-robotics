4ef28d3e6e89b4a431df1347a49f0af9f7c7c539
Mounika Alluri <mounika.alluri11@gmail.com>
2018-07-12 19:26:32 -0400

    Create Pose.py

---
+++ SLAM/Pose.py
@@ -0,0 +1,12 @@
+"""
+Pose with attributes x, y, theta 
+(polar co-ordinates of location of the particle)
+"""
+
+
+class Pose():
+
+	def __init__(self,x,y,t):
+		self.x = x
+		self.y = y
+		self.theta = t
